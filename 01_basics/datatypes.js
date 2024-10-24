5ddea8ac34e7f7e6c049b952a2ffda1206f1af3d
Charan Sai Setti <shetty9639@gmail.com>
2024-10-24 10:43:50 +0000

    datatypes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as new version
+
+// alert(3 + 3) // we are using noejs, not browser
+
+let name = 'charan'
+let age = 18
+let isLoggedIn = false
+
+// number 
+// bigint 
+
+// string
+
+// boolean
+
+// null -> standalone value (representation of an empty value)
+// undefined 
+// symbol -> used for uniqueness
+
+// object
+
+console.log(typeof 'charan'); // string
+
+console.log(typeof age); // number
+
+console.log(typeof null); // object
+
+console.log(typeof undefined); // undefined
