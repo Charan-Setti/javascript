c7a4624383e81c6729062aec258a600ac454f6aa
Charan Sai Setti <shetty9639@gmail.com>
2024-10-24 10:43:11 +0000

    variables

---
+++ 01_basics/variables.js
@@ -0,0 +1,26 @@
+const accountId = 123
+let accountEmail = 'charan@gmail.com'
+var accountPassword = '456'
+accountCity = 'Delhi'
+
+// we can declare the variables in javascript in the above mentioned ways
+
+let accountState;
+
+// accountId = 3 // not allowed because we are using const "const" for accountId
+
+accountEmail = 'chne@gmail.com'
+accountPassword = '234'
+accountCity = 'Hyderabad'
+
+
+/*
+
+Prefer not to use "var"
+because of issue in block scope and functional scope
+
+*/
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
