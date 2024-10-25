933b8a62ac21d440f12dcb4c511e58b614fef76c
Charan Sai Setti <shetty9639@gmail.com>
2024-10-25 09:48:52 +0000

    conversion of datatypes

---
+++ 01_basics/conversionOperation.js
@@ -0,0 +1,57 @@
+let score = '45cn'
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number (score)
+
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+/* 
+Conversion:
+
+String to NUmber:- 
+-> In JS if 45 is declared as string "45" it is converted into number as 45 (its type is number and if we try to print we get 45)
+    -- "45" => 45
+-> and also if we convert this string "45cn" it gets converted into number (its type is number but if we try to print it we get NaN)
+    -- "45cn" => NaN
+
+null to Number:-
+-> null converts into number (its type is number and if we try to print it gives 0)
+    -- null => 0
+
+undefined to Number:
+-> undefined converts into number 
+    -- undefined => NaN
+
+boolean to Number:
+    -- true => 1
+    -- false => 0
+
+
+*/
+
+let isLoggedIn = undefined
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+
+Boolean to Other type:
+
+    -- 1 => true; 0 => false
+    -- "" => false
+    -- "cn" => true
+    -- null => false
+    -- undefined => false
+*/
+
+let number = 45.9
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
+
